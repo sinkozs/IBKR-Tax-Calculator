1bc246a04f098d6b52184b875c1fce8d5b59496c
sinkozs <zsofia.sinko@outlook.com>
2024-05-30 21:05:00 +0200

    modify year filter

--- main.py
+++ main.py
@@ -30,58 +30,57 @@ def parse_pdf():
     return tax, div
 
 
+def filter_by_year(input_list):
+    filtered_list = [x for x in input_list if int(x[0]) == args.year[0]]
+    return filtered_list
+
+
 def calc_tax(tax):
     tax_sum_usd = 0.0
     tax_sum_huf = 0.0
-    tax_len = 0
 
     for t in tax:
-        if int(t[0]) == args.year[0]:
-            tax_len += 1
-            year = int(t[0])
-            month = int(t[1])
-            day = int(t[2])
-            usdv = float(t[3])
-            tax_sum_usd += usdv
-            exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
-            if not exchange_rate:
-                print(f"exchange rate error - {year}-{month}-{day}, trying again with - {year}-{month}-{day+1}")
-                day += 1
-            exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
-            uds2huf_rate = exchange_rate[0].rates[0].rate
-            tax_sum_huf += usdv * uds2huf_rate
+        year = int(t[0])
+        month = int(t[1])
+        day = int(t[2])
+        usdv = float(t[3])
+        tax_sum_usd += usdv
+        exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
+        if not exchange_rate:
+            print(f"exchange rate error - {year}-{month}-{day}, trying again with - {year}-{month}-{day+1}")
+            day += 1
+        exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
+        uds2huf_rate = exchange_rate[0].rates[0].rate
+        tax_sum_huf += usdv * uds2huf_rate
         time.sleep(0.1)
 
     print(f"tax [USD]: {tax_sum_usd}")
     print(f"tax [HUF]: {tax_sum_huf}")
-    print(f"tax transactions: {tax_len}")
+    print(f"tax transactions: {len(tax)}")
 
 
 def calc_div(div):
     div_sum_usd = 0.0
     div_sum_huf = 0.0
-    div_len = 0
 
     for d in div:
-        if int(d[0]) == args.year[0]:
-            div_len += 1
-            year = int(d[0])
-            month = int(d[1])
-            day = int(d[2])
-            usdv = float(d[3])
-            div_sum_usd += usdv
-            exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
-            if not exchange_rate:
-                print(f"exchange rate error - {year}-{month}-{day}, trying again with - {year}-{month}-{day+1}")
-                day += 1
-            exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
-            uds2huf_rate = exchange_rate[0].rates[0].rate
-            div_sum_huf += usdv * uds2huf_rate
+        year = int(d[0])
+        month = int(d[1])
+        day = int(d[2])
+        usdv = float(d[3])
+        div_sum_usd += usdv
+        exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
+        if not exchange_rate:
+            print(f"exchange rate error - {year}-{month}-{day}, trying again with - {year}-{month}-{day+1}")
+            day += 1
+        exchange_rate = client.get_exchange_rates(datetime.date(year, month, day), datetime.date(year, month, day), ["USD"])
+        uds2huf_rate = exchange_rate[0].rates[0].rate
+        div_sum_huf += usdv * uds2huf_rate
         time.sleep(0.1)
 
     print(f"div [USD]: {div_sum_usd}")
     print(f"div [HUF]: {div_sum_huf}")
-    print(f"div transactions: {div_len}")
+    print(f"div transactions: {len(div)}")
 
 
 if __name__ == "__main__":
@@ -95,5 +94,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     tax, div = parse_pdf()
+    tax = filter_by_year(tax)
+    div = filter_by_year(div)
     calc_tax(tax)
     calc_div(div)
